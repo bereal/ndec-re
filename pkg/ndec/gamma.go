cfb9673fa64f4653a3d9fcd30281e42a1377daf3
Alexander Berezhnoy <alexander@noice.com>
2024-03-15 11:01:10 +0200

    Make ApplyGamma bi-directional

--- pkg/ndec/gamma.go
+++ pkg/ndec/gamma.go
@@ -4,15 +4,6 @@ import "math/bits"
 
 const gammaIters = 0x7d
 
-func RotateRight(b byte, n uint) byte {
-	n = n & 7
-	if n == 0 {
-		return b
-	}
-
-	return b>>n | b<<(8-n)
-}
-
 func Gamma(password []byte) []byte {
 	data := make([]byte, 0xff)
 	copy(data, password)
@@ -46,12 +37,20 @@ func GammaHash(gamma []byte) byte {
 	return hash
 }
 
-func ApplyGamma(data, gamma []byte) {
+type Direction byte
+
+const (
+	Encode Direction = 1
+	Decode Direction = 0xff // -1
+)
+
+func ApplyGamma(data, gamma []byte, encode Direction) {
 	var xor, sum byte
 	for _, b := range gamma {
 		xor ^= b
 		sum += b
 	}
+	sum *= byte(encode)
 
 	for i, b := range data {
 		switch i % 3 {
--- pkg/ndec/gamma_test.go
+++ pkg/ndec/gamma_test.go
@@ -7,44 +7,50 @@ import (
 )
 
 func TestGamma(t *testing.T) {
-	password := []byte("password")
-	gamma := Gamma(password)
+	t.Run("generate gamma", func(t *testing.T) {
+		password := []byte("password")
+		gamma := Gamma(password)
 
-	expected := cleanupHex(`
-		71cbefc12baa7a7fe7dc9d9f7e71173f7db435ee196a5e7397ff16
-		ce4e305b2606974f5637f780350036ecb4aa0c9b1b9f9d268a6d9a
-		26ab042895b48958621da069826c60fdd36ebb1d84506309c0c78d
-		69ab86c7176036a57d742af74530a4fd6d4908e22eff8d2b6edaca
-		a3a380a16211bb2240d103cb3a4f2cb396
-	`)
+		expected := cleanupHex(`
+			71cbefc12baa7a7fe7dc9d9f7e71173f7db435ee196a5e7397ff16
+			ce4e305b2606974f5637f780350036ecb4aa0c9b1b9f9d268a6d9a
+			26ab042895b48958621da069826c60fdd36ebb1d84506309c0c78d
+			69ab86c7176036a57d742af74530a4fd6d4908e22eff8d2b6edaca
+			a3a380a16211bb2240d103cb3a4f2cb396
+		`)
 
-	h := hex.EncodeToString(gamma[:gammaIters])
-	if h != cleanupHex(expected) {
-		t.Errorf("Expected gamma to be %s, but got %s", expected, h)
-	}
-}
+		h := hex.EncodeToString(gamma[:gammaIters])
+		if h != cleanupHex(expected) {
+			t.Errorf("Expected gamma to be %s, but got %s", expected, h)
+		}
+	})
 
-func TestHash(t *testing.T) {
-	data := []byte("password")
+	t.Run("gamma hash", func(t *testing.T) {
+		data := []byte("password")
+		gamma := Gamma(data)
+		hash := GammaHash(gamma)
 
-	gamma := Gamma(data)
-	hash := GammaHash(gamma)
+		if hash != 0xac {
+			t.Errorf("Expected hash to be 0xac, but got %02x", hash)
+		}
+	})
 
-	if hash != 0xac {
-		t.Errorf("Expected hash to be 0xac, but got %02x", hash)
-	}
+	t.Run("apply gamma", func(t *testing.T) {
+		gamma := Gamma([]byte("password"))
+		data := []byte("test")
+		ApplyGamma(data, gamma, Encode)
+		expected := `647f5964`
+		if hex.EncodeToString(data) != expected {
+			t.Errorf("Expected data to be %s, but got %s", expected, hex.EncodeToString(data))
+		}
+
+		ApplyGamma(data, gamma, Decode)
+		if string(data) != "test" {
+			t.Errorf("Expected decoded data to be test, but got %s", data)
+		}
+	})
 }
 
 func cleanupHex(s string) string {
 	return regexp.MustCompile(`[^0-9a-fA-F]`).ReplaceAllString(s, "")
-}
-
-func TestApplyGamma(t *testing.T) {
-	gamma := Gamma([]byte("password"))
-	data := []byte("test")
-	ApplyGamma(data, gamma)
-	expected := `647f5964`
-	if hex.EncodeToString(data) != expected {
-		t.Errorf("Expected data to be %s, but got %s", expected, hex.EncodeToString(data))
-	}
 }
